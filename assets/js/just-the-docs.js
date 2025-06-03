be6eb52be1d4ca61821c1047c4a185a8f7b827e8
Ünal Dana <unal.dana@gmail.com>
2025-06-03 10:02:07 +0300

    Update site contents

--- assets/js/just-the-docs.js
+++ assets/js/just-the-docs.js
@@ -1,5 +1,3 @@
----
----
 (function (jtd, undefined) {
 
 // Event handling
@@ -54,8 +52,6 @@ function initNav() {
       menuButton.ariaPressed = false;
     }
   });
-
-  {%- if site.search_enabled != false and site.search.button %}
   const searchInput = document.getElementById('search-input');
   const searchButton = document.getElementById('search-button');
 
@@ -65,7 +61,6 @@ function initNav() {
     mainHeader.classList.add('nav-open');
     searchInput.focus();
   });
-  {%- endif %}
 }
 
 // The <head> element is assumed to include the following stylesheets:
@@ -87,38 +82,32 @@ function disableHeadStyleSheets() {
     activation.disabled = true;
   }
 }
-
-{%- if site.search_enabled != false %}
 // Site search
 
 function initSearch() {
   var request = new XMLHttpRequest();
-  request.open('GET', '{{ "assets/js/search-data.json" | relative_url }}', true);
+  request.open('GET', '/home/assets/js/search-data.json', true);
 
   request.onload = function(){
     if (request.status >= 200 && request.status < 400) {
       var docs = JSON.parse(request.responseText);
 
-      lunr.tokenizer.separator = {{ site.search.tokenizer_separator | default: site.search_tokenizer_separator | default: "/[\s\-/]+/" }}
+      lunr.tokenizer.separator = /[\s/]+/
 
       var index = lunr(function(){
         this.ref('id');
         this.field('title', { boost: 200 });
         this.field('content', { boost: 2 });
-        {%- if site.search.rel_url != false %}
         this.field('relUrl');
-        {%- endif %}
         this.metadataWhitelist = ['position']
 
         for (var i in docs) {
-          {% include lunr/custom-index.js %}
+          
           this.add({
             id: i,
             title: docs[i].title,
             content: docs[i].content,
-            {%- if site.search.rel_url != false %}
             relUrl: docs[i].relUrl
-            {%- endif %}
           });
         }
       });
@@ -282,7 +271,7 @@ function searchLoaded(index, docs) {
             var previewEnd = position[0] + position[1];
             var ellipsesBefore = true;
             var ellipsesAfter = true;
-            for (var k = 0; k < {{ site.search.preview_words_before | default: 5 }}; k++) {
+            for (var k = 0; k < 5; k++) {
               var nextSpace = doc.content.lastIndexOf(' ', previewStart - 2);
               var nextDot = doc.content.lastIndexOf('. ', previewStart - 2);
               if ((nextDot >= 0) && (nextDot > nextSpace)) {
@@ -297,7 +286,7 @@ function searchLoaded(index, docs) {
               }
               previewStart = nextSpace + 1;
             }
-            for (var k = 0; k < {{ site.search.preview_words_after | default: 10 }}; k++) {
+            for (var k = 0; k < 10; k++) {
               var nextSpace = doc.content.indexOf(' ', previewEnd + 1);
               var nextDot = doc.content.indexOf('. ', previewEnd + 1);
               if ((nextDot >= 0) && (nextDot < nextSpace)) {
@@ -357,7 +346,7 @@ function searchLoaded(index, docs) {
         resultLink.appendChild(resultPreviews);
 
         var content = doc.content;
-        for (var j = 0; j < Math.min(previewPositions.length, {{ site.search.previews | default: 3 }}); j++) {
+        for (var j = 0; j < Math.min(previewPositions.length, 3); j++) {
           var position = previewPositions[j];
 
           var resultPreview = document.createElement('div');
@@ -373,13 +362,10 @@ function searchLoaded(index, docs) {
           }
         }
       }
-
-      {%- if site.search.rel_url != false %}
       var resultRelUrl = document.createElement('span');
       resultRelUrl.classList.add('search-result-rel-url');
       resultRelUrl.innerText = doc.relUrl;
       resultTitle.appendChild(resultRelUrl);
-      {%- endif %}
     }
 
     function addHighlightedText(parent, text, start, end, positions) {
@@ -469,7 +455,6 @@ function searchLoaded(index, docs) {
     }
   });
 }
-{%- endif %}
 
 // Switch theme
 
@@ -480,7 +465,7 @@ jtd.getTheme = function() {
 
 jtd.setTheme = function(theme) {
   var cssFile = document.querySelector('[rel="stylesheet"]');
-  cssFile.setAttribute('href', '{{ "assets/css/just-the-docs-" | relative_url }}' + theme + '.css');
+  cssFile.setAttribute('href', '/home/assets/css/just-the-docs-' + theme + '.css');
 }
 
 // Note: pathname can have a trailing slash on a local jekyll server
@@ -544,17 +529,12 @@ function activateNav() {
 
 jtd.onReady(function(){
   initNav();
-  {%- if site.search_enabled != false %}
   initSearch();
-  {%- endif %}
   activateNav();
   scrollNav();
 });
 
 // Copy button on code
-
-
-{%- if site.enable_copy_code_button != false %}
 
 jtd.onReady(function(){
 
@@ -596,8 +576,6 @@ jtd.onReady(function(){
 
 });
 
-{%- endif %}
-
 })(window.jtd = window.jtd || {});
 
-{% include js/custom.js %}
+
